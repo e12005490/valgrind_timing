117292a3a94f843c173bdb53e4933c6b79570240
David <e12005490@student.tuwien.ac.at>
2021-04-27 00:09:56 +0200

    Update benchmark for easier stats gathering and fewer memory management problems

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -6,22 +6,177 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 
-#define SIZE_OF_STAT ((uint64_t)2000)
-#define BOUND_OF_LOOP ((uint64_t)5000)
+#define SAMPLE_SIZE ((uint64_t)1000000)
 
-#define SHIFT_AMOUNT 3
+typedef struct {
+	uint64_t time;
+	uint64_t occurrences;
+} time_pair;
 
-void inline fill_times(uint64_t **times) {
-	unsigned long flags;
+typedef struct {
+	uint64_t allocated_size;
+	uint64_t size;
+	time_pair *times;
+} time_map;
+
+/**
+ * Returns whether the initialization was successful
+ */
+int time_map_init(time_map *const map) {
+	map->allocated_size = 10;
+	map->size = 0;
+	map->times = kmalloc(map->allocated_size * sizeof(time_pair), GFP_KERNEL);
+	if(map->times == NULL) { return 0; }
+	return 1;
+}
+
+/**
+ * Returns whether inserting a time into a time_map was successful
+ */
+int time_map_insert(time_map *map, uint64_t time) {
+	uint64_t i;
+	uint64_t new_size;
+	time_pair *tmp;
+	time_pair swp;
+
+	for(i = 0; i < map->size; ++i) {
+		if(map->times[i].time == time) {
+			++map->times[i].occurrences;
+			return 1;
+		}
+	}
+
+	// not in map yet
+	if(map->size == map->allocated_size) {
+		new_size = 2 * map->allocated_size;
+		tmp = krealloc(map->times, new_size * sizeof(time_pair), GFP_KERNEL);
+		if(tmp == NULL) {
+			return 0;
+		}
+		map->times = tmp;
+		map->allocated_size = new_size;
+	}
+
+	// insert new pair
+	map->times[map->size].time = time;
+	map->times[map->size].occurrences = 1;
+	++map->size;
+
+	// (insertion) sort times
+	for(i = map->size - 1; i > 0 && map->times[i - 1].time > map->times[i].time; --i) {
+		swp.time = map->times[i - 1].time;
+		swp.occurrences = map->times[i - 1].occurrences;
+		map->times[i - 1].time = map->times[i].time;
+		map->times[i - 1].occurrences = map->times[i].occurrences;
+		map->times[i].time = swp.time;
+		map->times[i].occurrences = swp.occurrences;
+	}
+
+	return 1;
+}
+
+void time_map_free(time_map *map) {
+	map->allocated_size = 0;
+	map->size = 0;
+	kfree(map->times);
+}
+
+uint64_t time_map_mean(time_map *map) {
+	uint64_t i;
+	uint64_t result = 0, n = 0;
+
+	for(i = 0; i < map->size; ++i) {
+		result += map->times[i].time * map->times[i].occurrences;
+		n += map->times[i].occurrences;
+	}
+
+	return result / n;
+}
+
+uint64_t time_map_variance(time_map *map, uint64_t mean) {
+	uint64_t i;
+	uint64_t result = 0, n = 0;
+
+	for(i = 0; i < map->size; ++i) {
+		result += (map->times[i].time - mean) * (map->times[i].time - mean) * map->times[i].occurrences;
+		n += map->times[i].occurrences;
+	}
+
+	return result / n;
+}
+
+uint64_t time_map_median(time_map *map) {
+	uint64_t length = 0, mid;
+	uint64_t i;
+
+	for(i = 0; i < map->size; ++i) {
+		length += map->times[i].occurrences;
+	}
+
+	mid = length / 2;
+
+	// find the mid point by iterating on groups
+	if(length % 2 == 0) {
+		for(i = 0; i < map->size; ++i) {
+			if(mid >= map->times[i].occurrences) { // not in this group, go to next
+				mid -= map->times[i].occurrences;
+			} else if(mid == map->times[i].occurrences - 1) { // between this one and the next
+				return (map->times[i].time + map->times[i + 1].time) / 2;
+			} else { // in this one
+				return map->times[i].time;
+			}
+		}
+	} else {
+		for(i = 0; i < map->size; ++i) {
+			if(mid >= map->times[i].occurrences) { // not in this one, go to next
+				mid -= map->times[i].occurrences;
+			} else { // in this one
+				return map->times[i].time;
+			}
+		}
+	}
+
+	// should never get there
+	return 0;
+}
+
+/**
+ * Median Absolute Deviation
+ */
+uint64_t time_map_mad(time_map *map, uint64_t median) {
 	uint64_t i, j;
+	time_map mad_map;
+	uint64_t mad;
+
+	time_map_init(&mad_map);
+
+	for(i = 0; i < map->size; ++i) {
+		for(j = 0; j < map->times[i].occurrences; ++j) {
+			time_map_insert(&mad_map, map->times[i].time > median ? map->times[i].time - median : median - map->times[i].time);
+		}
+	}
+
+	mad = time_map_median(&mad_map);
+
+	time_map_free(&mad_map);
+
+	return mad;
+}
+
+void inline fill_times(time_map *map) {
+	unsigned long flags;
+	uint64_t i;
 	uint64_t start, end;
 	unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
-	volatile int to_shift;
+	volatile int mul = 1;
+
+	// warmup
 
 	asm volatile ("CPUID\n\t"
 		"RDTSC\n\t"
 		"mov %%edx, %0\n\t"
 		"mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+	asm volatile ("mul %0\n\t":: "r" (mul) : "%eax");
 	asm volatile ("RDTSCP\n\t"
 		"mov %%edx, %0\n\t"
 		"mov %%eax, %1\n\t"
@@ -30,124 +185,70 @@ void inline fill_times(uint64_t **times) {
 		"RDTSC\n\t"
 		"mov %%edx, %0\n\t"
 		"mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+	asm volatile ("mul %0\n\t":: "r" (mul) : "%eax");
 	asm volatile ("RDTSCP\n\t"
 		"mov %%edx, %0\n\t"
 		"mov %%eax, %1\n\t"
 		"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");
 
-	for(i = 0; i < BOUND_OF_LOOP; ++i) {
-		for(j = 0; j < SIZE_OF_STAT; ++j) {
-			preempt_disable();
-			raw_local_irq_save(flags);
+	for(i = 0; i < SAMPLE_SIZE; ++i) {
+		preempt_disable();
+		raw_local_irq_save(flags);
 
-			asm volatile ("CPUID\n\t"
-				"RDTSC\n\t"
-				"mov %%edx, %0\n\t"
-				"mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
-			/***********************************/
-			/*call the function to measure here*/
-			/***********************************/
-			to_shift <<= SHIFT_AMOUNT;
+		asm volatile ("CPUID\n\t"
+			"RDTSC\n\t"
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");
+		/***********************************/
+		/*call the function to measure here*/
 
-			asm volatile ("RDTSCP\n\t"
-				"mov %%edx, %0\n\t"
-				"mov %%eax, %1\n\t"
-				"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");
+		asm volatile ("mul %0\n\t":: "r" (mul) : "%eax");
 
-			raw_local_irq_restore(flags);
-			preempt_enable();
+		/***********************************/
+		asm volatile ("RDTSCP\n\t"
+			"mov %%edx, %0\n\t"
+			"mov %%eax, %1\n\t"
+			"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");
 
-			start = ((uint64_t)cycles_high << 32) | cycles_low;
-			end = ((uint64_t)cycles_high1 << 32) | cycles_low1;
+		raw_local_irq_restore(flags);
+		preempt_enable();
 
-			if((end - start) < 0) {
-				printk(KERN_ERR "\n\n>>>>>>>>>>>>>> CRITICAL ERROR IN TAKING THE TIME!!!!!!\n loop(%llu) stat(%llu) start = %llu, end = %llu\n", i, j, start, end);
-				times[i][j] = 0;
-			} else {
-				times[i][j] = end - start;
-			}
+		start = ((uint64_t)cycles_high << 32) | cycles_low;
+		end = ((uint64_t)cycles_high1 << 32) | cycles_low1;
+
+		if(!time_map_insert(map, end - start)) {
+			printk(KERN_ERR "\n\ncould not insert time\n");
 		}
 	}
-}
-
-uint64_t mean(uint64_t **times) {
-	uint64_t i, j;
-	uint64_t res = 0, prev = 0;
-
-	for(i = 0; i < BOUND_OF_LOOP; ++i) {
-		for(j = 0; j < SIZE_OF_STAT; ++j) {
-			if(res < prev) {
-				printk(KERN_ERR "\n\n>>>>>>>>>>>>>> CRITICAL OVERFLOW ERROR IN mean!!!!!!\n\n");
-				return -EINVAL;
-			}
-			res += times[i][j];
-			prev = res;
-		}
-	}
-
-	return res / (BOUND_OF_LOOP * SIZE_OF_STAT);
-}
-
-/* using König's formula */
-uint64_t variance(uint64_t **times) {
-	uint64_t i, j;
-	uint64_t e_t_squared = 0, e_squared_t = 0, prev_e_t_squared = 0, prev_e_squared_t = 0;
-	uint64_t size = BOUND_OF_LOOP * SIZE_OF_STAT;
-
-	for(i = 0; i < BOUND_OF_LOOP; ++i) {
-		for(j = 0; j < SIZE_OF_STAT; ++j) {
-			if((e_squared_t < prev_e_squared_t) || (e_t_squared < prev_e_t_squared)) { goto overflow; }
-			e_squared_t += times[i][j];
-			prev_e_squared_t = e_squared_t;
-			e_t_squared += times[i][j] * times[i][j];
-			prev_e_t_squared = e_t_squared;
-		}
-	}
-
-	e_squared_t *= e_squared_t;
-	e_t_squared *= size;
-	if((e_squared_t < prev_e_squared_t) || (e_t_squared < prev_e_t_squared)) { goto overflow; }
-
-	return (e_t_squared - e_squared_t) / (size * size);
-
-overflow:
-	printk(KERN_ERR "\n\n>>>>>>>>>>>>>> CRITICAL OVERFLOW ERROR IN var_calc!!!!!!\n\n");
-	return -EINVAL;
 }
 
 static int __init benchmark_start(void) {
-	uint64_t i = 0, j = 0;
-	uint64_t **times;
+	uint64_t i;
+	time_map map;
+	uint64_t mean, variance, median;
 
 	printk(KERN_INFO "Loading benchmark module\n");
 
-	times = kmalloc(BOUND_OF_LOOP * sizeof(uint64_t*), GFP_KERNEL);
-	if(!times) {
-		printk(KERN_ERR "unable to allocate memory for times\n");
-		return 0;
+	time_map_init(&map);
+
+	fill_times(&map);
+
+	mean = time_map_mean(&map);
+	variance = time_map_variance(&map, mean);
+	median = time_map_median(&map);
+
+	printk(KERN_ERR "mean: %llu", mean);
+	printk(KERN_ERR "variance: %llu", variance);
+	printk(KERN_ERR "median: %llu", median);
+	printk(KERN_ERR "mad: %llu", time_map_mad(&map, median));
+
+	printk("map size: %llu", map.size);
+	for(i = 0; i < map.size; ++i) {
+		printk("%llu, %llu", map.times[i].time, map.times[i].occurrences);
 	}
 
-	for(i = 0; i < BOUND_OF_LOOP; ++i) {
-		times[i] = kmalloc(SIZE_OF_STAT * sizeof(uint64_t), GFP_KERNEL);
-		if(!times[i]) {
-			printk(KERN_ERR "unable to allocate memory for times[%llu]\n", j);
-			for(j = 0; j < i; ++j) {
-				kfree(times[j]);
-			}
-			return 0;
-		}
-	}
+	time_map_free(&map);
 
-	fill_times(times);
-
-	printk(KERN_ERR "shift %d", SHIFT_AMOUNT);
-	printk(KERN_ERR "mean: %llu", mean(times));
-	printk(KERN_ERR "variance: %llu", variance(times));
-
-	for(i = 0; i < BOUND_OF_LOOP; ++i) {
-		kfree(times[i]);
-	}
-	kfree(times);
 	return 0;
 }
 
